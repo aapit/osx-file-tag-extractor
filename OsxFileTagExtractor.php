1c5643663731ea9d5be2dbf3eacaa9d6a622bf88
David Spreekmeester <david@grrr.nl>
2015-04-09 12:39:51 +0200

    First version of Extractor class, and a simple implementation

---
+++ OsxFileTagExtractor.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @author David Spreekmeester
+ * @year 2015
+ *
+ */
+class OsxFileTagExtractor {
+	protected $_ignorePaths = array('.', '..');
+
+
+	public function listFilesWithTags($dirPath) {
+		$Directory = new RecursiveDirectoryIterator($dirPath);
+		$Iterator = new RecursiveIteratorIterator($Directory);
+
+		foreach ($Iterator as $path) {
+			$this->_processNode($path);
+		}
+	}
+
+	protected function _processNode($path) {
+		$filename = basename($path);
+
+		if (in_array($filename, $this->_ignorePaths)) {
+			return;
+		}
+
+		echo "\033[1;34mProcessing $filename\033[m\n";
+
+		$command = $this->_composeAttrExtractionShellCommand($path);
+
+		unset($xmlTags);
+		exec($command, $xmlTags, $returnStatus);
+		if ($returnStatus === 1) {
+			return;
+		}
+
+		$xmlString = implode($xmlTags);
+
+		$xml = new SimpleXMLElement($xmlString);
+		$result = $xml->xpath('/plist/array');
+		print_r($result[0]->string) . "\n";
+	}
+
+	protected function _composeAttrExtractionShellCommand($path) {
+		return
+			'xattr -p com.apple.metadata:kMDItemOMUserTags "'
+			. str_replace(
+				array('"', '$'),
+				array('\"', '\$'),
+				$path
+			)
+			. '" 2> /dev/null | xxd -r -p'
+			. '| plutil -convert xml1 -o - -'
+		;
+	}
+}
---
+++ extract.php
@@ -0,0 +1,12 @@
+<?php
+require_once('OsxFileTagExtractor.php');
+
+if (!array_key_exists(1, $argv)) {
+	echo "Please provide the path to process as the first argument";
+	exit(1);
+}
+
+$path = $argv[1];
+
+$list = new OsxFileTagExtractor();
+$list->listFilesWithTags($path);
