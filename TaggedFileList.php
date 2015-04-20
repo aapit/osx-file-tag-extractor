b1be3820388de747d635e2aa14e039bb044663df
David Spreekmeester <david@grrr.nl>
2015-04-20 17:23:58 +0200

    Added output class

---
+++ TaggedFileList.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author David Spreekmeester
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ *
+ */
+class TaggedFileList extends ArrayObject {
+	
+	public function addNode(array $node = null) {
+		if ($node) {
+			$this[] = $node;
+		}
+	}
+
+}
