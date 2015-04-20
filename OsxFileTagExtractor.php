b228b0a574e6bca328ddc7af655494eb285f25e2
David Spreekmeester <david@grrr.nl>
2015-04-20 17:24:25 +0200

    Elaborated listing method, refactoring

--- OsxFileTagExtractor.php
+++ OsxFileTagExtractor.php
@@ -2,32 +2,59 @@
 /**
  * @author David Spreekmeester
  * @license http://opensource.org/licenses/BSD-3-Clause
- *
+ * @requires TaggedFileList
  */
+require_once "TaggedFileList.php";
+
+
 class OsxFileTagExtractor {
 	protected $_ignorePaths = array('.', '..');
 
 
+	/**
+ 	 * Lists the files in a given directory, with their metatags.
+ 	 * @param $dirPath The directory path, with or without trailing slash.
+ 	 * @return TaggedFileList A list of the nodes (files / dirs) in the directory, with their tags.
+ 	 */
 	public function listFilesWithTags($dirPath) {
+		$list = new TaggedFileList;
 		$Directory = new RecursiveDirectoryIterator($dirPath);
 		$Iterator = new RecursiveIteratorIterator($Directory);
 
 		foreach ($Iterator as $path) {
-			$this->_processNode($path);
+			$node = $this->_extractNodeTags($path);
+			$list->addNode($node);
 		}
+
+		return $list;
 	}
 
-	protected function _processNode($path) {
+	/**
+ 	 * Processes a node (file or directory) in the directory and extracts the tags.
+ 	 * @return Array An associative array with filename as key, and a numeric array containing tags.
+ 	 */
+	protected function _extractNodeTags($path) {
 		$filename = basename($path);
 
 		if (in_array($filename, $this->_ignorePaths)) {
 			return;
 		}
 
-		echo "\033[1;34mProcessing $filename\033[m\n";
-
 		$command = $this->_composeAttrExtractionShellCommand($path);
 
+		$tags = $this->_executeXmlTagsExtractor($command);
+
+		return array(
+			$filename => $tags
+		);
+	}
+
+	/**
+ 	 * Executes xml tag extractor, and converts output to plain array.
+ 	 * @param String $command The shell command to extract tags with.
+ 	 * @return Array An array with the tags, or null if no tags are available or there was an error.
+ 	 */
+	protected function _executeXmlTagsExtractor($command) {
 		unset($xmlTags);
 		exec($command, $xmlTags, $returnStatus);
 		if ($returnStatus === 1) {
@@ -38,7 +65,8 @@ class OsxFileTagExtractor {
 
 		$xml = new SimpleXMLElement($xmlString);
 		$result = $xml->xpath('/plist/array');
-		print_r($result[0]->string) . "\n";
+
+		return (array)$result[0]->string;
 	}
 
 	protected function _composeAttrExtractionShellCommand($path) {
