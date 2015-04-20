1a8aedf5c6b9fbf1c3ab7034df094cab94419b2a
David Spreekmeester <david@grrr.nl>
2015-04-20 17:58:34 +0200

    updated class location

--- extract.php
+++ extract.php
@@ -4,7 +4,7 @@
  * @license http://opensource.org/licenses/BSD-3-Clause
  *
  */
-require_once('OsxFileTagExtractor.php');
+require_once 'lib/OsxFileTagExtractor.php';
 
 if (!array_key_exists(1, $argv)) {
 	echo "Please provide the path to process as the first argument";
